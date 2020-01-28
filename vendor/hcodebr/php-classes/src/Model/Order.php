03071c3ff17fb1a5b89896b58de2d3aa62ca1f1c
Alex Botelho <alexbotelho1@hotmail.com>
2020-01-28 16:13:35 -0300

    Criado Botão de Busca Criado Paginação de Admin Pedidos

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -142,4 +142,39 @@ class Order extends Model
 
     }
 
+    public static function getPage($page, $search, $itemsPerPage)
+    {
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        ($search != "") ? $likes = "WHERE a.idorder = :id OR f.desperson LIKE :search" : $likes = " ";
+
+        $sql = new Sql();
+
+        $resultUsers = $sql->select("
+
+            SELECT SQL_CALC_FOUND_ROWS * 
+            FROM tb_orders a 
+            INNER JOIN tb_ordersstatus b USING(idstatus)
+            INNER JOIN tb_carts c USING(idcart)
+            INNER JOIN tb_users d ON d.iduser = a.iduser
+            INNER JOIN tb_addresses e USING(idaddress) 
+            INNER JOIN tb_persons f ON f.idperson = d.idperson
+            $likes
+            ORDER BY a.dtregister DESC
+            LIMIT $start, $itemsPerPage
+        ", [
+            ":search" => "%" . $search . "%",
+            ":id" => $search
+        ]);
+
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
+
+        return [
+            'data' => $resultUsers,
+            'total' => (int)$resultTotal[0]['nrtotal'],
+            'pages' => ceil($resultTotal[0]['nrtotal'] / $itemsPerPage)
+        ];
+
+    }
 }
